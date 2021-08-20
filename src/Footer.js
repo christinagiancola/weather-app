066fa9465523ca47c578dc97b8a53a7c93e7c2fb
Christina Giancola <christinamgiancola@gmail.com>
2021-08-20 11:21:59 -0400

    initial weather app set up with basic components, their css modules, and react-bootstrap

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <h3 className={styles.TextColor}>Built using Open Weather API</h3>
+  );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <h2 className={styles.TextColor}>My Weather App</h2>
+  );
+}
+
+export default Header;
