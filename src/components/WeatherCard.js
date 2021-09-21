f7416a24560b3428ce7606140a548f13411c4b64
Christina Giancola <christinamgiancola@gmail.com>
2021-09-20 20:33:28 -0400

    Import useState and useEffect into WeatherCard Component

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,7 +1,13 @@
+import { useState, useEffect } from 'react';
 import styles from './WeatherCard.module.css';
 import Card from 'react-bootstrap/Card';
 
 function WeatherCard() {
+  const [location, setLocation] = useState("Richmond");
+  const [weather, setWeather] = useState(["WEATHER"]);
+
+  
+
   return (
     <div className={styles.card}>
       <Card>
@@ -9,6 +15,7 @@ function WeatherCard() {
         <hr/>
         <p className={styles.text}>Current Temp: 70&deg;F</p>
         <p className={styles.text}>Max Temp: 85&deg;F</p>
+        <p className={styles.text}>Testing: {weather}</p>
       </Card>
     </div>
   );
