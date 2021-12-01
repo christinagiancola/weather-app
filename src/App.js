54d34ffbe26f2873f160b4d2ae92d2d8d248629d
Christina Giancola <christinamgiancola@gmail.com>
2021-12-01 08:50:12 -0500

    Fixed bug that caused location to be reset to defaultLocation between new forecastAPI calls

--- src/App.js
+++ src/App.js
@@ -6,24 +6,17 @@ import SearchBar from './components/SearchBar/SearchBar';
 import WeatherCards from './components/WeatherCards/WeatherCards';
 import Footer from './components/Footer/Footer.js';
 
-
 function App() {
   const [forecast, setForecast] = useState(null);
   const [defaultLocation, setDefaultLocation] = useState("");
 
   useEffect(() => {
-    //check if geolocationAPI is availabe in user's browser
-    console.log(`App useEffect triggered`);
     if (window.navigator.geolocation) {
-      console.log(`geolocationAPI request made for defaultLocation`);
       navigator.geolocation.getCurrentPosition((pos) => {
         setDefaultLocation(`${pos.coords.latitude},${pos.coords.longitude}`)}, (err) => console.log(`Geolocation lookup failed: ${err}`));
-        console.log(`defaultLocation set: ${defaultLocation}`);
      };
   }, [defaultLocation])
 
-
- 
   return (
     <div className={styles.body}>
       <Header />
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,35 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap';
 import styles from './SearchBar.module.css';
 
 function SearchBar({ defaultLocation, setForecast }) {
   const WEATHER_API_KEY = 'e684477a6ce04c44b0c180151212009';
+  const hasSetDefaultLocation = useRef(false);
   const [inputText, setInputText] = useState("");
   const [inputLocation, setInputLocation] = useState("");
 
   const handleSubmit = (e) => {
-    console.log(`SearchBar handleSubmit triggered`);
-    // when user submits SearchBar form, inputLocation is updated which should trigger a new forecastAPI call
     e.preventDefault();
     setInputLocation(inputText);
   };
 
   useEffect(() => {
-    console.log(`SearchBar useEffect triggered`);
-    console.log(`inputLocation: ${inputLocation}`);
-    setInputLocation(defaultLocation);
+    if (!hasSetDefaultLocation.current && defaultLocation) {
+      setInputLocation(defaultLocation);
+      hasSetDefaultLocation.current = true;
+    }
     if (inputLocation !== "") {
-      console.log(`SearchBar useEffect triggered`);
       const getForecast = async () => {
-        console.log(`SearchBar getForecast called`);
         let response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${inputLocation}&days=3&aqi=yes&alerts=yes`);
         setForecast(response.data);
       };
       getForecast();
     }
   }, [setForecast, inputLocation, defaultLocation]); 
-
 
   return (
     <div>
