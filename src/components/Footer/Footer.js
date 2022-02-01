14c870ce62d5d612c705ec5b9dcce35f2766d32e
Christina Giancola <christinamgiancola@gmail.com>
2022-01-31 19:11:23 -0500

    Adjust contrast in Header to improve readability

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
     <div className={styles.container}>
       <div className={styles.gradient}/>
       <div className={styles.footer}>
-        <h4 className={styles.text} id='footerHeading'>App created by Christina Giancola</h4>
-        <h4 className={styles.text} id='footerSubheading'>Forecasts powered by <a href="https://www.weatherapi.com/" title="Weather API" className={styles.link}>WeatherAPI.com</a></h4>
+        <h3 className={styles.text} id='footerHeading'>App created by Christina Giancola</h3>
+        <h3 className={styles.text} id='footerSubheading'>Forecasts powered by <a href="https://www.weatherapi.com/" title="Weather API" className={styles.link}>WeatherAPI.com</a></h3>
       </div>
     </div>
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,8 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <div className={styles.container}>
-      <h4 className={styles.text + ' ' + styles.header}>simple weather</h4>
-      <h3 className={styles.text + ' ' + styles.subheader}>forecasts for today, tonight, and tomorrow</h3>
+      <h1 className={styles.text + ' ' + styles.header}>simple weather</h1>
+      <h2 className={styles.text + ' ' + styles.subheader}>forecasts for today, tonight, and tomorrow</h2>
     </div>
   );
 }
