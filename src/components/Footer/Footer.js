3fe56c84b18a30898f4ece86beeda07a6d0547a1
Christina Giancola <christinamgiancola@gmail.com>
2022-05-01 13:31:36 -0400

    Updated <Footer> to make name link to github profile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,25 @@
 import styles from './Footer.module.css';
 
 function Footer() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.gradient}/>
-      <div className={styles.footer}>
-        <h3 className={styles.text} id='footerHeading'>App created by Christina Giancola</h3>
-        <h3 className={styles.text} id='footerSubheading'>Forecasts powered by <a href="https://www.weatherapi.com/" title="Weather API" className={styles.link}>WeatherAPI.com</a></h3>
-      </div>
-    </div>
-
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.gradient} />
+			<div className={styles.footer}>
+				<h3 className={styles.text} id='footerHeading'>
+					App created by{' '}
+					<a href='https://github.com/christinagiancola' title='Christina Giancola' className={styles.link}>
+						Christina Giancola
+					</a>
+				</h3>
+				<h3 className={styles.text} id='footerSubheading'>
+					Forecasts powered by{' '}
+					<a href='https://www.weatherapi.com/' title='Weather API' className={styles.link}>
+						WeatherAPI.com
+					</a>
+				</h3>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
